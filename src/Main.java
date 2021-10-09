b16df01ecfa81a07f0800139f66cee18d394b121
Alizhan Kenesbay <alzhnk@gmail.com>
2021-10-09 17:43:38 +0600

    Practice

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] array = new int[8];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10) + 1;
+            if (i % 2 != 0) {
+                array[i] = 0;
+            }
+        }
+        System.out.print(Arrays.toString(array));
+    }
+}
